4cb69ba273951402ef8fee5bb60256205766f4d0
Perseu <josimar.17perseu@gmail.com>
2021-06-25 13:35:36 -0300

    commite com declaração de variavel, alert, e console

---
+++ main.js
@@ -0,0 +1,10 @@
+ 
+ var nome = "Josimar Souza "; //tipagem dinamica não precisa definir o tipo de mensagem
+ var idade = 25;
+ 
+ alert(nome + "tem " + idade + " anos");// cria um alerta com um pop up de mensagem na tela
+ 
+ //consele.log(nome); //mostra no console apertando inspensiona pagina o que a variavel contem
+ alert(nome.replace("Josimar", "Perseu")); //replace troca josimar por perseu, dunciona no alert como no console
+ 
+ 
